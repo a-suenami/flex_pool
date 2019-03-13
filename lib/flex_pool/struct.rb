104e4fef1d3060d29415c5fcba7616dab6a66ac6
Akira SUENAMI <a.suenami@gmail.com>
2019-03-13 21:55:51 +0900

    Implement Struct class.

---
+++ lib/flex_pool/struct.rb
@@ -0,0 +1,36 @@
+module FlexPool
+  module Struct
+    def self.new(*args, &block)
+      klass = Class.new do
+        @@attributes = []
+
+        class << self
+          def set_attrributes(*attributes)
+            @@attributes ||= []
+            @@attributes.concat(attributes)
+            attr_reader(*attributes)
+          end
+        end
+
+        set_attrributes(*args)
+
+        def initialize(attributes)
+          shortage_attributes = @@attributes - attributes.keys
+          if shortage_attributes.any?
+            raise ArgumentError, "Some attributes doesn't given: #{shortage_attributes}"
+          end
+
+          attributes.each do |name, value|
+            instance_variable_set(:"@#{name}", value)
+          end
+        end
+      end
+
+      if block_given?
+        klass.class_eval(&block)
+      end
+
+      klass
+    end
+  end
+end
---
+++ spec/flex_pool/struct_spec.rb
@@ -0,0 +1,31 @@
+require 'flex_pool/struct'
+
+RSpec.describe FlexPool::Struct do
+  let(:klass) { FlexPool::Struct.new(:id, :name) }
+
+  it 'has some attributes and each getters' do
+    object = klass.new(id: 1, name: 'Suenami Akira')
+    expect(object.id).to eq(1)
+    expect(object.name).to eq('Suenami Akira')
+  end
+
+  it 'doesn\'t have setters (complete constructor pattern)' do
+    object = klass.new(id: 1, name: 'Suenami Akira')
+    expect{ object.id = 2 }.to raise_error(NoMethodError)
+    expect{ object.name = 'Yamada Taro' }.to raise_error(NoMethodError)
+  end
+
+  it 'can\'t construct without any attributes' do
+    expect { klass.new(id: 1) }.to raise_error(ArgumentError, 'Some attributes doesn\'t given: [:name]')
+  end
+
+  it 'can define any instance methods' do
+    klass = FlexPool::Struct.new(:id, :first_name, :last_name) do
+      def full_name
+        first_name + ' ' + last_name
+      end
+    end
+    object = klass.new(id: 1, first_name: 'Akira', last_name: 'Suenami')
+    expect(object.full_name).to eq('Akira Suenami')
+  end
+end
