550d1ac6fc178523a65d57413725fac9fb045f25
Akira SUENAMI <a.suenami@gmail.com>
2019-03-14 08:29:48 +0900

    Implement mapper a little.

--- lib/flex_pool/mapper.rb
+++ lib/flex_pool/mapper.rb
@@ -1,7 +1,46 @@
+require 'flex_pool/struct'
+
 module FlexPool
   class Mapper
-    def self.map(relation, limit: nil, map_type:)
-      [map_type.new(1, 'Akira Suenami')]
+    class << self
+      def mapping_attributes(*mapping_attributes)
+        new(mapping_attributes: mapping_attributes)
+      end
+
+      def mapping_type(mapping_type)
+        new(mapping_type: mapping_type)
+      end
+    end
+
+    def initialize(mapping_attributes: [], mapping_type: nil)
+      @mapping_attributes = mapping_attributes
+      @mapping_type = mapping_type
+    end
+
+    def mapping_attributes(*mapping_attributes)
+      new_attribues = (@mapping_attributes + mapping_attributes).uniq
+      self.class.new(mapping_attributes: new_attribues, mapping_type: @mapping_type)
+    end
+
+    def mapping_type(mapping_type)
+      self.class.new(mapping_attributes: @mapping_attributes, mapping_type: mapping_type)
+    end
+
+    def map(relation, limit: nil)
+      # Just fake it
+      [current_mapping_type.new(id: 1, name: 'Akira Suenami')]
+
+      # relation.limit(limit).pluck(attributes).map do |record|
+      #   mapping_type.new(record)
+      # end
+    end
+
+    def current_mapping_type
+      @mapping_type || FlexPool::Struct.new(*current_mapping_attributes)
+    end
+
+    def current_mapping_attributes
+      @mapping_attributes
     end
   end
 end
